14f304c53d26c703db71031767127ce606395810
Priyagupta108 <priyagupta108@github.com>
2025-09-03 11:12:43 +0530

    test

--- functions/a.ts
+++ functions/a.ts
@@ -1,1 +1,13 @@
-let x:
+const a = (a: number, b: number): number => a + b;
+
+export const handler = () => {
+  make javascript fail again
+  async function foo(things) {
+    const results = [];
+    for (const thing of things) {
+      // Bad: each loop iteration is delayed until the entire asynchronous operation completes
+      results.push(await bar(thing));
+    }
+    return baz(results);
+  }
+};
